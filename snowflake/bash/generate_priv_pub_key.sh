8822a7db830a0cc6e5061db7134e231e7db39443
radik.safin <safinrraf@gmail.com>
2025-01-26 21:52:57 +0100

    forgot files

--- snowflake/bash/generate_priv_pub_key.sh
+++ snowflake/bash/generate_priv_pub_key.sh
@@ -3,10 +3,10 @@
 cd ~/.ssh
 
 #generate private key
-openssl genrsa 2048 | openssl pkcs8 -topk8 -v2 des3 -inform PEM -out rsa_key_snowflake_hrvgkue_sh32424_terraform_user.p8
+openssl genrsa 2048 | openssl pkcs8 -topk8 -v2 des3 -inform PEM -out rsa_key_snowflake_[SNOWFLAKE_ACCOUNT]_terraform_user.p8
 
 #generate pub key based on the private key
-openssl rsa -in rsa_key_snowflake_hrvgkue_sh32424_terraform_user.p8 -pubout -out rsa_key_snowflake_hrvgkue_sh32424_terraform_user.pub
+openssl rsa -in rsa_key_snowflake_[SNOWFLAKE_ACCOUNT]_terraform_user.p8 -pubout -out rsa_key_snowflake_[SNOWFLAKE_ACCOUNT]_terraform_user.pub
 
 #veriyfy the fingerprint
-openssl rsa -pubin -in rsa_key_snowflake_hrvgkue_sh32424_terraform_user.pub -outform DER | openssl dgst -sha256 -binary | openssl enc -base64
+openssl rsa -pubin -in rsa_key_snowflake_[SNOWFLAKE_ACCOUNT]_terraform_user.pub -outform DER | openssl dgst -sha256 -binary | openssl enc -base64
