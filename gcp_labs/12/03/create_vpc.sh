9d09b6bdba83efb06c75b733c5889f39845509af
radik.safin <safinrraf@gmail.com>
2025-01-29 08:38:25 +0100

    lab#12

--- gcp_labs/12/03/create_vpc.sh
+++ gcp_labs/12/03/create_vpc.sh
@@ -84,3 +84,21 @@ gcloud compute instances create privatenet-vm-1 --zone=us-east1-c --machine-type
 gcloud compute instances list --sort-by=ZONE
 
 #Create the VM instance with multiple network interfaces
+gcloud compute instances create vm-appliance \
+    --project=qwiklabs-gcp-02-9530d43788dc \
+    --zone=us-east1-c \
+    --machine-type=e2-standard-4 \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=privatesubnet-1 \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=managementsubnet-1 \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=mynetwork \
+    --metadata=enable-oslogin=true \
+    --maintenance-policy=MIGRATE \
+    --provisioning-model=STANDARD \
+    --service-account=859347128234-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
+    --create-disk=auto-delete=yes,boot=yes,device-name=vm-appliance,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250113,mode=rw,size=10,type=pd-balanced \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
